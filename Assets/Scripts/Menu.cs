57ded1193dc9b54f91f81ed4428632bd4b156801
Monito <phil.menard1668@gmail.com>
2026-02-20 16:22:26 +0100

    Added options menu
    Implemented options menu that display commands and a slider for volume that still need to be implemented. Need to try all levels to see if nothing is broken

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,12 +8,15 @@ public class Menu : MonoBehaviour
 {
 	[SerializeField] private InputActionAsset inputActions;
 	[SerializeField] private RectTransform panel;
+	[SerializeField] private RectTransform optionsPanel;
 	[SerializeField] private Button play;
 	[SerializeField] private Button options;
 	[SerializeField] private Button quit;
+	[SerializeField] private Button back;
 
 	private InputAction menuAction;
 	public bool isPaused;
+	public bool inControls = false;
 	private bool hasStarted;
 	private string currentScene;
 	
@@ -45,21 +48,36 @@ public class Menu : MonoBehaviour
 		play.onClick.AddListener(() => onButtonClicked(1));
 		options.onClick.AddListener(() => onButtonClicked(2));
 		quit.onClick.AddListener(() => onButtonClicked(3));
+		back.onClick.AddListener(() => onButtonClicked(4));
 	}
 
 	private void Update()
 	{
 		if (menuAction.WasPressedThisFrame() && hasStarted)
+		{
 			isPaused = !isPaused;
+			if (inControls)
+				inControls = false;
+		}
 
 		if (isPaused)
 		{
-			panel.gameObject.SetActive(true);
+			if (inControls)
+			{
+				panel.gameObject.SetActive(false);
+				optionsPanel.gameObject.SetActive(true);
+			}
+			else
+			{
+				panel.gameObject.SetActive(true);
+				optionsPanel.gameObject.SetActive(false);
+			}
 			Time.timeScale = 0;
 		}
 		else
 		{
 			panel.gameObject.SetActive(false);
+			optionsPanel.gameObject.SetActive(false);
 			Time.timeScale = 1;
 		}
 	}
@@ -74,6 +92,9 @@ public class Menu : MonoBehaviour
 			isPaused = false;
 		}
 
+		if (x == 2)
+			inControls = true;
+
 		if (x == 3)
 		{
 			#if UNITY_EDITOR
@@ -82,5 +103,8 @@ public class Menu : MonoBehaviour
 				Application.Quit();
 			#endif
 		}
+
+		if (x == 4)
+			inControls = false;
 	}
 }
